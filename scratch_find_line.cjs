b358a54d7a060ced637407a363dbe877e2c1f552
BeingSazzad <sazzad.uiuxdesign@gmail.com>
2026-09-16 06:03:16 +0600

    chore: cleanup scratch files

--- scratch_find_line.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('index.html', 'utf8');
-const lines = html.split('\n');
-lines.forEach((l, i) => {
-  if (l.includes('id="screen-bookingDetails"')) {
-    console.log('Found screen-bookingDetails on line:', i + 1);
-  }
-});
